8f1d55379340695934eb9245c6d7699bb5f0c1a7
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-06-04 17:49:20 +0100

    integrating and using mongodb to store the blog entries

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import datetime
+import os
+from pymongo import MongoClient
 
-load_dotenv()
-
-app = Flask(__name__)
-
-entries = []
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.insert(0, (entry_content, formatted_date))
-    
-    entries_with_date = [
-        (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
-        )
-        for entry in entries
-    ]
-    return render_template("home.html", entries = entries_with_date)
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        client = MongoClient(os.getenv("MONGODB_URI"))
+        app.db = client.microblog
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+        
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("home.html", entries = entries_with_date)
+    return app
