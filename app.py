242efcb51b730b4bc4529c41db9f552d4a88bb3e
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-06-04 09:19:32 +0100

    adding new card with blog entry from list (not a DB)

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
+entries = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+    
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+    return render_template("home.html", entries = entries_with_date)
