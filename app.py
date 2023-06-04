450a930c77bdd0bb39a4b224fe55f868901cfc6e
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-06-04 21:28:02 +0100

    jinja setting VALUE attribute of html button (even within quotes!). HTML form attribute NAME is how the form is referenced and attribute VALUE is what's returned.
    Python distinguishing between different buttons via POSTS message received.
    
    Adding mongoDB document id to the HTML button's value attribute.

--- app.py
+++ app.py
@@ -13,18 +13,27 @@ def create_app():
         client = MongoClient(os.getenv("MONGODB_URI"))
         app.db = client.microblog
         if request.method == "POST":
-            entry_content = request.form.get("content")
-            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+            if request.form.get("Submit") == "submit":
+                entry_content = request.form.get("content")
+                formatted_date = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+                app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+            
+            else:
+                print("\n\n\n\n\n")
+                print(request.form.get("Delete"))
+                print("\n\n\n\n\n\n\n")
         
-        entries_with_date = [(entry["content"], entry["date"]) for entry in app.db.entries.find({})]
-        sorted_by_date = sorted(entries_with_date, key=lambda tup: tup[1], reverse=True)
+        entries_with_date_and_dbID = [
+            (entry["content"], entry["date"], entry["_id"]) for entry in app.db.entries.find({})
+        ]
+        entries_with_date_and_dbID_sorted = sorted(
+            entries_with_date_and_dbID, key=lambda tup: tup[1], reverse=True)
 
         # app.db.entries.delete_one({"content": "hippo"})
         my_collection = app.db.entries.find().sort("content")
         for my_doc in my_collection:
             pprint.pprint(my_doc)
 
-        return render_template("home.html", entries = sorted_by_date)
+        return render_template("home.html", entries = entries_with_date_and_dbID_sorted)
 
     return app
